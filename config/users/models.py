9ea35b4f8ef8d4183c85852c347d00904b5aff36
seoeunbi <seoeunbi25@naver.com>
2024-09-26 11:04:56 +0900

    fix: 유저 모델 수정

--- config/users/models.py
+++ config/users/models.py
@@ -3,23 +3,19 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, username, password):
-        if not email:
-            raise ValueError('User must have an email address')
+    def create_user(self, username, password):
         if not username:
             raise ValueError('User must have a username')
         
         user = self.model(
-            email=self.normalize_email(email),
             username=username,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, username, password):
         user = self.create_user(
-            email,
             username=username,
             password=password,
         )
@@ -28,19 +24,42 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
-    
-    email = models.EmailField(
-        verbose_name='email address',
+
+    username = models.CharField(
+        verbose_name='username',
         max_length=255,
         unique=True,
     )
-    username = models.CharField(max_length=255)
+
+    kakao = models.CharField(
+        verbose_name='kakao',
+        max_length=255,
+        unique=True,
+    )
+
+    naver = models.CharField(
+        verbose_name='naver',
+        max_length=255,
+        unique=True,
+    )
+
+    google = models.CharField(
+        verbose_name='google',
+        max_length=255,
+        unique=True,
+    )
+
+    github = models.CharField(
+        verbose_name='github',
+        max_length=255,
+        unique=True,
+    )
+
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
