e9ec8f11b841ce3dee0ed63bf69e3e46083c3351
seoeunbi <seoeunbi25@naver.com>
2024-09-26 10:43:46 +0900

    create: 유저 모델 생성

--- config/users/models.py
+++ config/users/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, username, password):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not username:
+            raise ValueError('User must have a username')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
